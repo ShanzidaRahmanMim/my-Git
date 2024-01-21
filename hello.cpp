695ff77b60eef1389bad5fc32430398a5b11ff2b
ShanzidaRahmanMim <shanzidamim33@gmail.com>
2024-01-22 02:08:48 +0600

    modified code

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+int addit(int a,int b)
+    {
+        return a+b;
+    }
 int main()
 {
-    cout<<"Hello world";
+    cout<<"Hello world"<<endl;
+    cout<<addit(5,6);
 }
